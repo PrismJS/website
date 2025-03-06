87e44ec876119baa84fa62952901047441c94486
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-03-06 14:15:21 +0100

    FIrst fixes (#1)
    * Fix URLs: relative → `https://prismjs.com`
    
    * Re-add logo font

--- assets/code.js
+++ assets/code.js
@@ -172,9 +172,9 @@ var setTheme;
 	}
 
 	setTheme = function (id) {
-		var link = $$('link[href^="themes/prism"]')[0];
+		var link = $$('link[href^="https://prismjs.com/themes/prism"]')[0];
 
-		link.href = themes.meta.path.replace(/\{id\}/g, id);
+		link.href = ("https://prismjs.com/" + themes.meta.path).replace(/\{id\}/g, id);
 		localStorage.setItem('theme', id);
 	};
 
