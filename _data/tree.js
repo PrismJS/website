19efd602b969aea8b82a808a19d4856f3cbc4d35
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-03-20 22:10:57 +0100

    Add todo

--- _data/tree.js
+++ _data/tree.js
@@ -2,6 +2,7 @@ import Fetch from "@11ty/eleventy-fetch";
 
 export default async () => {
 	let repo = await Fetch(
+		// TODO: Replace with "https://api.github.com/repos/PrismJS/prism/git/trees/main?recursive=1" when v2 is launched
 		"https://api.github.com/repos/PrismJS/prism/git/trees/master?recursive=1",
 		{
 			duration: "1d",
