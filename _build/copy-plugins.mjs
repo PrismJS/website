38e3d2d657113bf98b0457030910a1b973e8b855
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-05-07 23:30:18 +0200

    Add copy script

---
+++ _build/copy-plugins.mjs
@@ -0,0 +1,47 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const sourcePath = path.resolve(__dirname, "../node_modules/prismjs/src/plugins");
+const destPath = path.resolve(__dirname, "../plugins");
+
+async function copy () {
+	// We need { recursive: true } so the script doesn't fail if the folder already exists
+	await fs.mkdir(destPath, { recursive: true });
+
+	let plugins = await fs.readdir(sourcePath, { withFileTypes: true });
+	for (let plugin of plugins) {
+		if (!plugin.isDirectory()) {
+			continue;
+		}
+
+		let source = path.join(sourcePath, plugin.name);
+		let dest = path.join(destPath, plugin.name);
+		await fs.mkdir(dest, { recursive: true });
+
+		let files = await fs.readdir(source, { withFileTypes: true });
+		for (let file of files) {
+			if (!file.isFile()) {
+				continue;
+			}
+
+			let name = path.parse(file.name).name;
+			// Don't copy the plugin source files
+			if (!name.endsWith(plugin.name)) {
+				await fs.copyFile(path.join(source, file.name), path.join(dest, file.name));
+			}
+		}
+	}
+}
+
+await copy();
+
+// Create plugins.json in the plugins folder with global data
+let json = {
+	permalink: "{{ page.filePathStem.replace('README', '') }}/index.html",
+	tags: ["plugin"],
+};
+
+await fs.writeFile(path.join(destPath, "plugins.json"), JSON.stringify(json, null, "\t"));
